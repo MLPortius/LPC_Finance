0de7d0b219240f4f47c42f0ebfe03d7a10c7fc12
Andriu <mlportius@gmail.com>
2023-02-10 04:59:20 -0300

    data preparation done

--- data_preparation.py
+++ data_preparation.py
@@ -6,15 +6,15 @@ Created on Wed Feb  8 23:47:16 2023
 """
 
 import pandas as pd
-import pickle
 import compress_pickle as cpickle
 
 infolder = 'input/'
 outfolder = 'output/'
+plots = outfolder+'plots/'
 
-data = pd.read_excel(infolder+'sp500_market_data.xlsx')
+#%% INITIAL DATA
+data = pd.read_excel(infolder+'excel/'+'sp500_market_data.xlsx')
 slices_ind = list(data.columns)
-
 
 #%% TICKERS
 slices_tickers = {}
@@ -80,7 +80,7 @@ with open(infolder+'marketindex_data'+'.lzma','wb') as file:
 
 #%% FIXED INCOME
 
-data = pd.read_excel(infolder+'sp500_market_data.xlsx',sheet_name='fixed_income')
+data = pd.read_excel(infolder+'excel/'+'sp500_market_data.xlsx',sheet_name='fixed_income')
 data.set_index('Date',inplace=True,drop=True)
 
 bonds = list(data.columns)
@@ -107,12 +107,150 @@ del(b, cols, var, c, d)
 
 # COMPRESSED PICKLE
 with open(infolder+'fixedincome_data'+'.lzma','wb') as file:
-    cpickle.dump(data,file,compression='lzma')
+    cpickle.dump(fixed_data,file,compression='lzma')
 
 
-#%%
+#%% TICKER LIST
+
+with open(infolder+'cpickle/'+'tickers_data.lzma','rb') as file:
+    data_dict = cpickle.load(file)
+
+tickers = list(data_dict.keys())
+tickers.sort()
+
+# COMPRESSED PICKLE
+with open(infolder+'ticker_list'+'.lzma','wb') as file:
+    cpickle.dump(tickers,file,compression='lzma')
 
 
+#%% ORGANIZE DATAFRAME
+
+# Load data
+with open(infolder+'cpickle/'+'tickers_data.lzma','rb') as file:
+    data_dict = cpickle.load(file)
+
+with open(infolder+'cpickle/'+'ticker_list.lzma','rb') as file:
+    ticker_list = cpickle.load(file)
+
+del(file)
+
+# Ordered data
+filtered_data = {}
+for t in ticker_list:
+    d = data_dict[t]
+    filtered_data[t] = {'close':d['CLOSE'],'vol':d['VOLUME']}
+
+del(t, d)
+
+# Joining data
+close = pd.DataFrame(filtered_data[ticker_list[0]]['close'])
+vols = pd.DataFrame(filtered_data[ticker_list[0]]['vol'])
+
+for t in ticker_list[1:]:
+    
+    p = filtered_data[t]['close']
+    v = filtered_data[t]['vol']
+    
+    close = pd.concat([close,p],axis=1)
+    vols = pd.concat([vols,v],axis=1)
+    
+close.columns = ticker_list    
+vols.columns = ticker_list
+
+del(p, v, t)
+del(data_dict, filtered_data)
+    
+#%% FILTERING DATA
+
+import numpy as np
+
+mv = close.isna().sum(axis=0)
+mv.sort_values(ascending=False,inplace=True)
+
+nmv = mv[mv==0]
+filtered_list = list(nmv.index)
+filtered_list.sort()
+
+percentage = np.round(len(nmv)/len(mv)*100,2)
+
+# import plotly.express as px
+# from plotly.offline import plot as offplot
+# from IPython.display import Image
+
+# figsize=(2,1)
+# dpi = 720
+# fig = px.bar(mv2, title = 'SP500 - Close Price - Missing Values',width=figsize[0]*dpi,height=figsize[1]*dpi)
+# fig.update_layout(plot_bgcolor='white',showlegend=False)
+# fig.update_xaxes(showline=False,linecolor='black',showgrid=False,gridcolor='lightgrey',griddash='dash',title='Stock')
+# fig.update_yaxes(showline=False,linecolor='black',showgrid=True,gridcolor='lightgrey',griddash='dash',title='Missing Values')
+# offplot(fig)
+
+# fig.write_image(plots+'missingvalues'+'.png')
+# fig.write_image(plots+'missingvalues'+'.svg')
+# fig.write_html(plots+'missingvalues'+'.html')
+
+# # bytes
+# img_bytes = fig.to_image(format="png")
+# Image(img_bytes)
+
+close_filtered = close.loc[:,filtered_list]
+vols_filtered = vols.loc[:,filtered_list]
+
+
+#%% SPLITTING DATA
+
+def splitter(df, ratio=0.5):
+
+    N = len(df.index)
+    n = int(np.round(N*0.5,0))
+    
+    s1 = df.iloc[:n,:]
+    s2 = df.iloc[n:,:]
+    
+    return s1, s2
+
+close_f1, close_f2 = splitter(close_filtered,ratio=0.5)
+vols_f1, vols_f2 = splitter(vols_filtered, ratio=0.5)
+
+def slicer(df, ns=10):
+    
+    N = len(df.columns)
+    n = int(np.round(N/ns,0))
+    
+    slices = []
+    for i in range(ns):
+        print('slice',i+1,'...')
+        s = df.iloc[:,i*n:(i+1)*n]
+        slices.append(s)
+    
+    return slices
+
+slices_cf1 = slicer(close_f1, ns=10)
+slices_cf2 = slicer(close_f2, ns=10)
+slices_vf1 = slicer(vols_f1, ns=10)
+slices_vf2 = slicer(vols_f2, ns=10)
+
+#%% JOIN AND SAVE DATA
+
+dataset1 = []
+for i in range(len(slices_cf1)):
+    dataset1.append({'close':slices_cf1[i],'vols':slices_vf1[i]}) 
+    
+dataset2 = []
+for i in range(len(slices_cf2)):
+    dataset2.append({'close':slices_cf2[i],'vols':slices_vf2[i]}) 
+
+data = {'set1':dataset1, 'set2':dataset2}
+
+stickers = []
+for s in slices_cf1:
+    stickers.append(list(s.columns))
     
 
+# COMPRESSED PICKLE
 
+with open(infolder+'lpc_datasets'+'.lzma','wb') as file:
+    cpickle.dump(data,file,compression='lzma')
+    
+with open(infolder+'slice_tickers'+'.lzma','wb') as file:
+    cpickle.dump(data,file,compression='lzma')
